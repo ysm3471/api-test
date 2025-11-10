bf8aba0fbabfda44297e702223cfc512fa3ebdb0
YouSangMoon <122746595+ysm3471@users.noreply.github.com>
2025-11-10 20:08:08 +0900

    cd/cd test2

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // 간단한 샘플 엔드포인트
 router.get('/hello', (req, res) => {
-  res.json({ hello: 'world' });
+  res.json({ hello: 'world!' });
 });
 
 module.exports = router;
